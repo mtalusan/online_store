98f1ef3c472c4c3c812d5c6be0fc6b33e06678d4
Michael Talusan <z1714949@hopper.cs.niu.edu>
2022-04-28 19:38:48 -0500

    Added table that prints all details of the product

--- item_page.php
+++ item_page.php
@@ -11,11 +11,60 @@
 	openMariaDB($username, $password);
 
 	// Get item information
-	$rs = pdo->prepare("SELECT * FROM Product WHERE Product_ID = :item");
-	$rs->execute(array(":item" => $item));
-	$item_info = $rs->fetchAll(PDO::FETCH_ASSOC);
+	$item_info_rs = pdo->prepare("SELECT * FROM Product WHERE Product_ID = :item");
+	$item_info_rs->execute(array(":item" => $item));
+	$item_info = $item_info_rs->fetchAll(PDO::FETCH_ASSOC);
 
+	// Print item details
 	echo "<table border=2 cellspacing=2>";
 	echo "<tr><td><b>Product Name</b></td>";
-	echo "<td>" . $item["Product_Name"] . "</td></tr>";
+	echo "<td>" . $item_info["Product_Name"] . "</td></tr>";
+	echo "<tr><td><b>Details</b></td>";
+	echo "<td>" . $item_info["Details"] . "</td></tr>";
+	echo "<tr><td><b>Price</b></td>";
+	echo "<td>" . $item_info["Base_Price"] . "</td></tr>";
+
+
+	// Checking if size are applicable and printing if so
+	echo "<tr><td><b>Sizes</b></td>";
+	if($item_info["Size"] != NULL)
+	{
+		// Run another query if there is size
+		$item_sizes_rs = pdo->prepare("SELECT Size FROM Product WHERE Product_Name = :item_name");
+		$item_sizes_rs->execute(array(":item_name" => $item_info["Product_Name"]));
+		$item_sizes = $item_size_rs->fetchAll(PDO::FETCH_ASSOC);
+
+		echo "<td>";
+		foreach($item_sizes as $item_size)
+		{
+			echo $item_sizes["Size"] . "<br />";
+		}
+		echo "</td></tr>";
+	}
+	else
+	{
+		echo "<td>N/A</td></tr>";
+	}
+
+
+	// Checking color if applicable and printing if so
+	echo "<tr><td><b>Colors</b></td>";
+	if($item_info["Color"] != NULL)
+	{
+		// Run another query if there is color
+		$item_colors_rs = pdo->prepare("SELECT Color FROM Product WHERE Product_Name = :item_name");
+		$item_colors_rs->execute(array(":item_name" => $item_info["Product_Name"]));
+		$item_colors = $item_colors_rs->fetchAll(PDO::FETCH_ASSOC);
+
+		echo "<td>";
+		foreach($item_colors as $item_color)
+		{
+			echo $item_colors["Color"] . "<br />";
+		}
+		echo "</td></tr>";
+	}
+	else
+	{
+		echo "<td>N/A</td></tr>";
+	}
 ?>
