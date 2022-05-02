e7df525f3d2ded34b0e6510a425b7d74f76cba23
Michael Talusan <z1714949@hopper.cs.niu.edu>
2022-05-02 17:03:12 -0500

    Added dropboxes to select color and size if applicable and a button to link to a new page to confirm addition to cart

--- item_page.php
+++ item_page.php
@@ -1,5 +1,7 @@
 <html>
 <?php
+	echo "<a href=\"index.php\">Home</a><br /><br />";
+
 	include("credentials.php");
 
 	try
@@ -14,6 +16,7 @@
 	}
 
 	$item_ID = $_POST["item"];
+	@$C_ID = $_POST["Customer_ID"];
 
 	// Get item information
 	$item_info_rs = $pdo->prepare("SELECT * FROM Product WHERE Product_ID = :item;");
@@ -43,6 +46,7 @@
 		$item_sizes_rs->execute(array(":item_name" => $item_info["Product_Name"]));
 		$item_sizes = $item_sizes_rs->fetchAll(PDO::FETCH_ASSOC);
 
+		// Print sizes
 		echo "<td>";
 		foreach($item_sizes as $item_size)
 		{
@@ -67,6 +71,7 @@
 
 		echo "<td>";
 
+		// Print colors
 		$previous_color = NULL;
 		foreach($item_colors as $item_color)
 		{
@@ -87,7 +92,51 @@
 		echo "<td>N/A</td></tr>";
 	}
 
-	echo "<a href=\"index.php\">Home</a>";
+
+	// Select quantity, size or color options to submit to car
+	// Pass Product_Name, Customer_ID, and Product_ID
+	echo "<form action=\"add_to_cart_confirm.php\" method=\"POST\">";
+
+	// Select size
+	if($item_info["Size"] != NULL)
+	{
+		echo "<select name=\"Size\">";
+		foreach($item_sizes as $item_size)
+		{
+			echo "<option>" . $item_size["Size"] . "</option>";
+		}
+		echo "</select><br />";
+	}
+
+	// Select Color
+	$previous_item = NULL;
+	if($item_info["Color"] != NULL)
+	{
+		echo "<select name=\"Color\">";
+		foreach($item_colors as $item_color)
+		{
+			echo "<option>" . $item_color["Color"] . "</option>";
+			$previous_item = $item_color["Color"];
+		}
+		echo "</select><br />";
+	}
+
+	// Enter quantity
+	echo "<input type=\"text\" name=\"Quantity\" value=\"Enter Quantity Here\"/><br />";
+
+	// Pass Customer_ID if known
+	echo "<input type=\"hidden\" name=\"Customer_ID\" value=\"" . @$C_ID . "\"/>";
+
+	// Pass Product_ID
+	echo "<input type=\"hidden\" name=\"Product_ID\" value=\"" . $item_info["Product_ID"] . "\"/>";
+
+	// Pass Product_Name
+	echo "<input type=\"hidden\" name=\"Product_Name\" value=\"" . $item_info["Product_Name"] . "\"/>";
+
+	echo "<input type=\"submit\" value=\"Add to Cart\"/>";
+
+	echo "</form>";
 	echo "</body>";
+
 ?>
 </html>
