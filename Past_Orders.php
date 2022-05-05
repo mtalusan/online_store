890ecb93488bc828ee8592ff6a84734eeeedfcaa
Michael Talusan <z1714949@hopper.cs.niu.edu>
2022-05-05 10:15:13 -0500

    Added home button. Tracks user between pages

--- Past_Orders.php
+++ Past_Orders.php
@@ -7,16 +7,28 @@
 		$dsn = "mysql:host=courses;dbname=z1714949";
 		$pdo = new PDO($dsn,$username,$password);
 		$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-		
-		echo '<form action="Past_Orders.php" method="POST">';
-		echo "<input type='text' name='quicknote'/>\t";
-		echo "<input type='submit' name='submit'/>";
-		echo '</form>';
 
-		$Customer_ID = $_POST['quicknote'];
+		@$Customer_ID = @$_POST['Customer_ID'];
+
+		// Button to return to the home page
+		echo "<form action=\"index.php\" method=\"POST\">";
+		echo "<input type=\"hidden\" name=\"Customer_ID\" value=\"" . @$Customer_ID . "\"/>";
+		echo "<input type=\"submit\" value=\"Home\"/>";
+		echo "</form><br /><br />";
+
+
+		if(!@$Customer_ID)
+		{
+			echo '<form action="Past_Orders.php" method="POST">';
+			echo "<input type='text' name='Customer_ID'/>\t";
+			echo "<input type='submit' name='submit'/>";
+			echo '</form>';
+		}
+
+
 
 		$rs = $pdo->prepare("SELECT distinct Order_ID, Price FROM Order_Info where Processing_status = 'Delivered' and Customer_ID = ?");
-		$rs->execute(array($Customer_ID));
+		$rs->execute(array(@$Customer_ID));
 		$rows = $rs->fetchALL(PDO::FETCH_ASSOC);
 
 		if(empty($rows))
