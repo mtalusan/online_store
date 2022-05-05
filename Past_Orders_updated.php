8538722692a86de1e7b6050d2dfb745782689b0d
Ayeflo19 <102984893+Ayeflo19@users.noreply.github.com>
2022-05-05 03:50:58 -0500

    Add files via upload

---
+++ Past_Orders_updated.php
@@ -0,0 +1,54 @@
+<html><head><title>Past Orders</title></head><body><pre>
+<?php
+
+	include('secrets.php');
+
+	try{ // if something goes wrong, an exception is thrown
+		$dsn = "mysql:host=courses;dbname=z1960742";
+		$pdo = new PDO($dsn,$username,$password);
+		$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+		
+		echo '<form action="Past_Orders.php" method="POST">';
+		echo "<input type='text' name='quicknote'/>\t";
+		echo "<input type='submit' name='submit'/>";
+		echo '</form>';
+
+		$Customer_ID = $_POST['quicknote'];
+
+		$rs = $pdo->prepare("SELECT distinct Order_ID, Price FROM Order_Info where Processing_status = 'Delivered' and Customer_ID = ?");
+		$rs->execute(array($Customer_ID));
+		$rows = $rs->fetchALL(PDO::FETCH_ASSOC);
+
+		if(empty($rows))
+		{
+			echo "\n\t\tNo Past Orders Found for this Customer ID";
+		}
+		else
+		{
+
+			echo "<table border=1 cellspacing=10>";
+        		echo "<tr>";
+			foreach($rows[0] as $key => $item)
+			{	
+				echo "<th>$key</th>";
+			}
+			echo "</tr>";
+ 			foreach($rows as $row)
+			{
+				echo "<tr>";
+				foreach ($row as $key => $item)
+				{
+					echo "<td>$item</td>";
+				}
+				echo "</tr>";
+			}
+			echo "</table>";		
+		}
+	}
+
+	catch(PDOexception $e)
+	{
+		echo "Connection to database failed: " . $e->getMessage();
+	}
+?>	
+</pre></body></html>
---
+++ show_orders_updated.php
@@ -0,0 +1,59 @@
+<html><head><title>Orders</title></head><body><pre>
+<?php
+
+	include('secrets.php');
+
+	try{ // if something goes wrong, an exception is thrown
+		$dsn = "mysql:host=courses;dbname=z1960742";
+		$pdo = new PDO($dsn,$username,$password);
+		$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+		echo '<form action="show_orders.php" method="POST">';
+		echo "<input type='text' name='quicknote'/>\t";
+		echo "<input type='submit' name='submit'/>";
+		echo '</form>';
+	
+		$Customer_ID = $_POST['quicknote'];
+
+		$rs = $pdo->prepare("SELECT distinct Order_ID, Tracking_Number, Price FROM Order_Info where Customer_ID = ?");
+
+		$rs->execute(array($Customer_ID));
+		$rows = $rs->fetchALL(PDO::FETCH_ASSOC);
+
+		if(empty($rows))
+		{
+			echo "\n\n\t\tCustomer ID not found\n";
+		}
+		else
+		{
+			echo "<table border=1 cellspacing=10>";
+        		echo "<tr>";
+			foreach($rows[0] as $key => $item)
+			{	
+				echo "<th>$key</th>";
+			}
+			echo "</tr>";
+ 			foreach($rows as $row)
+			{	
+				echo "<tr>";
+				foreach($row as $item)
+				echo "<th>$item</th>";
+				echo "</tr>";
+				$Tprice = $Tprice + $row['Price'];
+			}
+			echo "</table>";	
+
+			echo "\n\n\tTotal Price = " . $Tprice;
+
+			echo "\n\n\t\t<a href='Order_Tracking.php'>Track Order</a>\n\n";
+			echo "\n\n\t\t<a href='Past_Orders.php'>Past Orders</a>\n\n";
+		}
+	}
+
+	catch(PDOexception $e)
+	{
+		echo "Connection to database failed: " . $e->getMessage();
+	}
+?>
+</pre></body></html>
+	
