c39bdcb6bad85a1da66710b3d16f15ced7fb1346
Michael Talusan <z1714949@hopper.cs.niu.edu>
2022-05-05 10:11:28 -0500

    Added a link to see_orders.php

--- admin_logon.php
+++ admin_logon.php
@@ -2,11 +2,18 @@
 <a href="index.php">Home</a><br />
 <?php
 
+	// Edit Inventory
 	echo "<form action=\"manage_inventory.php\" method=\"POST\"/>";
 	echo "Enter \"admin\" to access inventory management:<br />";
 	echo "<input type=\"text\" name=\"Admin_ID\"/>";
 	echo "<input type=\"submit\" value=\"Log On\">";
 	echo "</form>";
 
+	// Process Orders
+	echo "<form action=\"see_orders.php\" method=\"POST\"/>";
+	echo "Enter \"admino\" to access order management:<br />";
+	echo "<input type=\"text\" name=\"Admin_ID\"/>";
+	echo "<input type=\"submit\" value=\"Log On\">";
+	echo "</form>";
 ?>
 </html>
