5e38a288e88a719f325bedf337dcf53f0dbfc5aa
Michael Talusan <z1714949@hopper.cs.niu.edu>
2022-05-04 19:33:05 -0500

    Suppressed some errors

--- show_orders.php
+++ show_orders.php
@@ -1,19 +1,20 @@
 <html><head><title>Orders</title></head><body><pre>
 <?php
-
-	include('secrets.php');
+	echo "\n\n\t\t<a href='Order_Tracking.php'>Track Order</a>\n\n";
+	echo "\n\n\t\t<a href='Past_Orders.php'>Past Orders</a>\n\n";
+	include('credentials.php');
 
 	try{ // if something goes wrong, an exception is thrown
-		$dsn = "mysql:host=courses;dbname=z1960742";
+		$dsn = "mysql:host=courses;dbname=z1714949";
 		$pdo = new PDO($dsn,$username,$password);
 		$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
-		echo '<form action="http://students.cs.niu.edu/~z1960742/show_orders.php" method="POST">';
+		echo '<form action="http://students.cs.niu.edu/~z1714949/show_orders.php" method="POST">';
 		echo "<input type='text' name='quicknote'/>\t";
 		echo "<input type='submit' name='submit'/>";
 		echo '</form>';
 	
-		$Customer_ID = $_POST['quicknote'];
+		@$Customer_ID = @$_POST['quicknote'];
 
 		$rs = $pdo->prepare("SELECT distinct Order_ID, Tracking_Number, Price FROM Order_Info where Customer_ID = ?");
 
