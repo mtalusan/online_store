0b72fc663621d5c76e6e095a89047dd4d001812c
Ayeflo19 <102984893+Ayeflo19@users.noreply.github.com>
2022-05-05 04:25:57 -0500

    Update Past_Orders.php
    Added a customer id input part

--- Past_Orders.php
+++ Past_Orders.php
@@ -7,27 +7,43 @@
 		$dsn = "mysql:host=courses;dbname=z1714949";
 		$pdo = new PDO($dsn,$username,$password);
 		$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+		
+		echo '<form action="Past_Orders.php" method="POST">';
+		echo "<input type='text' name='quicknote'/>\t";
+		echo "<input type='submit' name='submit'/>";
+		echo '</form>';
 
-		$rs = $pdo->query("SELECT distinct Order_ID, Price FROM Order_Info where Processing_status = 'Delivered'");
+		$Customer_ID = $_POST['quicknote'];
+
+		$rs = $pdo->prepare("SELECT distinct Order_ID, Price FROM Order_Info where Processing_status = 'Delivered' and Customer_ID = ?");
+		$rs->execute(array($Customer_ID));
 		$rows = $rs->fetchALL(PDO::FETCH_ASSOC);
 
-		echo "<table border=1 cellspacing=10>";
-        	echo "<tr>";
-		foreach($rows[0] as $key => $item)
-		{	
-			echo "<th>$key</th>";
-		}
-		echo "</tr>";
- 		foreach($rows as $row)
+		if(empty($rows))
 		{
-			echo "<tr>";
-			foreach ($row as $key => $item)
-			{
-				echo "<td>$item</td>";
+			echo "\n\t\tNo Past Orders Found for this Customer ID";
+		}
+		else
+		{
+
+			echo "<table border=1 cellspacing=10>";
+        		echo "<tr>";
+			foreach($rows[0] as $key => $item)
+			{	
+				echo "<th>$key</th>";
 			}
 			echo "</tr>";
+ 			foreach($rows as $row)
+			{
+				echo "<tr>";
+				foreach ($row as $key => $item)
+				{
+					echo "<td>$item</td>";
+				}
+				echo "</tr>";
+			}
+			echo "</table>";		
 		}
-		echo "</table>";		
 	}
 
 	catch(PDOexception $e)
