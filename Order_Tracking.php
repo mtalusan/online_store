326375c4bb4074ca551dcca427b38cc53c567b81
Michael Talusan <z1714949@hopper.cs.niu.edu>
2022-05-05 10:14:44 -0500

    Added home button

--- Order_Tracking.php
+++ Order_Tracking.php
@@ -13,12 +13,20 @@
 		echo "Connection to database failed: " . $e->getMessage();
 	}
 
+	@$Customer_ID = @$_POST['Customer_ID'];
+
+	// Button to return to the home page
+	echo "<form action=\"index.php\" method=\"POST\">";
+	echo "<input type=\"hidden\" name=\"Customer_ID\" value=\"" . @$Customer_ID . "\"/>";
+	echo "<input type=\"submit\" value=\"Home\"/>";
+	echo "</form><br /><br />";
+
 
 	echo"\n\t\tPlease Enter the tracking number for the order\n\n";
 	
-	echo '<form action="Order_Tracking.php" method="POST">';
-	echo "<input type='text' name='quicknote'/>\t";
-	echo "<input type='submit' name='submit/'>";
+	echo '<form action="Order_Tracking.php\" method="POST">';
+	echo '<input type="text" name="quicknote"/>\t';
+	echo '<input type="submit" name="submit"/>';
 	echo '</form>';
 
 	@$Tracking_Number = @$_POST['quicknote'];
