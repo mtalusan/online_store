487e4053adfd91377b5afdd8c3604544613daf7d
Michael Talusan <z1714949@hopper.cs.niu.edu>
2022-05-05 10:13:33 -0500

    Changed "quicknote" to "Customer_ID". Supressed errors, tracks user between pages, added home button, added button to Order_Tracking.php

--- show_orders.php
+++ show_orders.php
@@ -8,16 +8,26 @@
 		$pdo = new PDO($dsn,$username,$password);
 		$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
-		echo '<form action="show_orders.php" method="POST">';
-		echo "<input type='text' name='quicknote'/>\t";
-		echo "<input type='submit' name='submit'/>";
-		echo '</form>';
-	
-		$Customer_ID = $_POST['quicknote'];
+		@$Customer_ID = @$_POST['Customer_ID'];
+
+		// Button to return to the home page
+		echo "<form action=\"index.php\" method=\"POST\">";
+		echo "<input type=\"hidden\" name=\"Customer_ID\" value=\"" . @$Customer_ID . "\"/>";
+		echo "<input type=\"submit\" value=\"Home\"/>";
+		echo "</form><br /><br />";
+
+
+		if(!@$Customer_ID)
+		{
+			echo '<form action="show_orders.php" method="POST">';
+			echo "<input type='text' name='Customer_ID'/>\t";
+			echo "<input type='submit' name='submit'/>";
+			echo '</form>';
+		}
 
 		$rs = $pdo->prepare("SELECT distinct Order_ID, Tracking_Number, Price FROM Order_Info where Customer_ID = ?");
 
-		$rs->execute(array($Customer_ID));
+		$rs->execute(array(@$Customer_ID));
 		$rows = $rs->fetchALL(PDO::FETCH_ASSOC);
 
 		if(empty($rows))
@@ -45,7 +55,13 @@
 
 			echo "\n\n\tTotal Price = " . $Tprice;
 
-			echo "\n\n\t\t<a href='Order_Tracking.php'>Track Order</a>\n\n";
+
+			// Button to Order Tracking
+			echo "<form action=\"Order_Tracking.php\" method=\"POST\">";
+			echo "<input type=\"hidden\" name=\"Customer_ID\" value=\"" . @$Customer_ID . "\"/>";
+			echo "<input type=\"submit\" value=\"Track an Order\"/>";
+			echo "</form><br /><br />";
+
 		}
 	}
 
