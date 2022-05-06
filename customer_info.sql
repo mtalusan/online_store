2486c607b9d1a7e73638967cce8ca1bbe3c61e08
Ayeflo19 <102984893+Ayeflo19@users.noreply.github.com>
2022-05-05 20:42:17 -0500

    Update customer_info.sql

--- customer_info.sql
+++ customer_info.sql
@@ -28,39 +28,13 @@ SET time_zone = "+00:00";
 -- Table structure for table `customer_info`
 --
 
-CREATE TABLE `customer_info` (
-  `id` int(11) NOT NULL,
-  `customer_id` int(11) NOT NULL,
-  `name` varchar(200) NOT NULL,
-  `email` varchar(200) NOT NULL,
-  `phone` varchar(200) NOT NULL,
-  `address` varchar(200) NOT NULL,
-  `city` varchar(200) NOT NULL,
-  `state` varchar(200) NOT NULL,
-  `zip` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE Customer(
+  `Customer_ID` int(11) auto_increment,
+  `Phone_Number` varchar(200),
+  `Shipping_Address` varchar(200) NOT NULL,
+  `Customer_Name` varchar(200) NOT NULL,
+  `Card_Number` varchar(200) NOT NULL,
+  
+  Primary Key(Customer_ID)
+);
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `customer_info`
---
-ALTER TABLE `customer_info`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `customer_info`
---
-ALTER TABLE `customer_info`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
