def26fb7e67f49bb530ad8d102af8e0db755a838
mtalusan <46937464+mtalusan@users.noreply.github.com>
2022-05-05 23:28:01 -0500

    Changed order

--- create_all_DB.sql
+++ create_all_DB.sql
@@ -6,56 +6,10 @@ DROP TABLE Customer;
 DROP TABLE Product;
 
 
--- Create Wishlist DB
-CREATE TABLE Wishlist (
-	Product_ID INT,
-	Customer_ID INT,
-	
-	PRIMARY KEY (Customer_ID),
-	FOREIGN KEY (Customer_ID) REFERENCES Customer(Customer_ID),
-	FOREIGN KEY (Product_ID) REFERENCES Product(Product_ID)
-);
-
--- Define Order Info DB
-create table Order_Info(
-    Order_ID INT not null,
-    Customer_ID char(10) not null,
-    Processing_status char(15) default 'Processing',
-    Seller_Notes char(100),
-    Tracking_Number int(10) AUTO_INCREMENT,
-    Price decimal(6,2) not null,
-    
-    Primary Key(Tracking_Number),
-    Foreign Key(Order_ID,Customer_ID, Price) References Ordered_Item(Order_ID, Price), Customer(Customer_ID)
-);
-
--- Define Ordered Item DB
-CREATE TABLE Ordered_Item (
-	Order_ID INT AUTO_INCREMENT,
-	Customer_ID INT,
-	Product_ID INT,
-	Quantity INT,
-	Price DECIMAL(6,2),
-
-	PRIMARY KEY (Order_ID),
-	FOREIGN KEY (Product_ID) REFERENCES Product(Product_ID), 
-	FOREIGN KEY (Customer_ID) REFERENCES Customer(Customer_ID)
-);
-
--- Define Cart DB
-CREATE TABLE Shopping_Cart (
-	Product_ID INT,
-	Customer_ID INT,
-	Quantity INT,
-
-	PRIMARY KEY (cart_id),
-	FOREIGN KEY (Product_ID) REFERENCES Product(Product_ID),
-	FOREIGN KEY (Customer_ID) REFERENCES Customer(Customer_ID)
-);
 
 -- Define Customer DB
 CREATE TABLE Customer (
-	Customer_ID INT AUTO_INCREMENT,
+	Customer_ID CHAR(255),
 	Customer_Name CHAR(255),
 	Phone_Number INT,
 	Shipping_Address CHAR(255),
@@ -77,3 +31,50 @@ CREATE TABLE Product (
 	PRIMARY KEY (Product_ID)
 );
 
+-- Define Ordered Item DB
+CREATE TABLE Ordered_Item (
+	Order_ID INT AUTO_INCREMENT,
+	Customer_ID CHAR(255),
+	Product_ID INT,
+	Quantity INT,
+	Price DECIMAL(6,2),
+
+	PRIMARY KEY (Order_ID),
+	FOREIGN KEY (Product_ID) REFERENCES Product(Product_ID), 
+	FOREIGN KEY (Customer_ID) REFERENCES Customer(Customer_ID)
+);
+
+-- Create Wishlist DB
+CREATE TABLE Wishlist (
+	Product_ID INT,
+	Customer_ID CHAR(255),
+	
+	PRIMARY KEY (Customer_ID),
+	FOREIGN KEY (Customer_ID) REFERENCES Customer(Customer_ID),
+	FOREIGN KEY (Product_ID) REFERENCES Product(Product_ID)
+);
+
+-- Define Order Info DB
+create table Order_Info (
+    Order_ID INT not null,
+    Customer_ID CHAR(255),
+    Processing_status char(15) default 'Processing',
+    Seller_Notes char(100),
+    Tracking_Number int(10) AUTO_INCREMENT,
+    Price decimal(6,2) not null,
+    
+    Primary Key(Tracking_Number),
+    FOREIGN KEY (Customer_ID) REFERENCES Customer(Customer_ID),
+    Foreign Key(Order_ID) References Ordered_Item(Order_ID)
+);
+
+-- Define Cart DB
+CREATE TABLE Shopping_Cart (
+	Product_ID INT,
+	Customer_ID CHAR(255),
+	Quantity INT,
+
+	PRIMARY KEY (Customer_ID),
+	FOREIGN KEY (Product_ID) REFERENCES Product(Product_ID),
+	FOREIGN KEY (Customer_ID) REFERENCES Customer(Customer_ID)
+);
