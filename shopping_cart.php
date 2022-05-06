90854d4c66ac57e8d271bea1ee5e156b674acc4b
Michael Talusan <z1714949@hopper.cs.niu.edu>
2022-05-05 22:34:08 -0500

    Renamed shopping_cart2.php to shopping_cart

--- shopping_cart.php
+++ shopping_cart.php
@@ -1,151 +1,67 @@
 <?php
 
+	echo "<form action=\"index.php\" method=\"POST\">";
+	echo "<input type=\"hidden\" name=\"Customer_ID\" value=\"" . $_POST["Customer_ID"] . "\"/>";
+	echo "<input type=\"submit\" value=\"Home\"/>";
+	echo "</form><br /><br />";
+	$customer_id = $_POST["Customer_ID"];
+	@$product_id = $_POST["Product_ID"];
+	@$quantity = @$_POST['Quantity'];
 
-$customer_id = $_POST["Customer_ID"];
-$product_id = $_POST["Product_ID"];
+	include("credentials.php");
 
-include("credentials.php");
+	$dsn = "mysql:host=courses;dbname=z1714949";
+	$pdo = new PDO($dsn, $username, $password);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
-$dsn = "mysql:host=courses;dbname=z1714949";
-$pdo = new PDO($dsn, $username, $password);
-$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+	
+	// Add to cart
+	if(@$quantity)
+	{
+		$sql = "INSERT INTO Shopping_Cart (Product_ID, Customer_ID, Quantity) VALUES (:Product_ID, :Customer_ID, :Quantity)";
+		$stmt= $pdo->prepare($sql);
+		$stmt->execute(array(':Product_ID' => $product_ID, ':Customer_ID' => $customer_ID, ':Quantity'=>$quantity));
+	}
+	else if(@$product_id)
+	{
+		// Remove from cart
+		$remove_rs = $pdo->prepare("DELETE FROM Shopping Cart WHERE Customer_ID = :Customer_ID AND Product_ID = :Product_ID");
+		$remove_rs->execute(array(":Customer_ID" => $customer_id, ":Product_ID" => @$product_id));
 
+	}
 
-if(!empty($_POST)){
-
-    if(!empty($_POST['submit'])){
-//echo "<pre>";print_r($_POST);die;
-if($_POST['submit'] == 'add_quantity'){
-    try
-    {
-    $quantity=($_POST['Quantity'] + 1);
-    $sql = "UPDATE Shopping_Cart SET quantity = :quantity WHERE cart_id = :cart_id ";
-    $stmt= $pdo->prepare($sql);
-    $stmt->execute([':quantity'=>$quantity,':cart_id'=>$cart_id]);
-    }
-    catch(PDOexception $e)
-    {
-        echo "Connection to database failed: " . $e->getMessage();
-    }
-
-} elseif($_POST['submit'] == 'remove'){
-    try
-    {
-
-        $quantity=($_POST['quantity'] - 1);
-        $cart_id=$_POST['cart_id'];
-
-        /*Remove product from cart if quantity is 0*/
-        if($quantity =='0'){
-            $sql = "DELETE FROM cart WHERE cart_id = :cart_id ";
-            $stmt= $pdo->prepare($sql);
-            $stmt->execute([':cart_id'=>$cart_id]);
-        }else{
-            $sql = "UPDATE cart SET quantity = :quantity WHERE cart_id = :cart_id ";
-            $stmt= $pdo->prepare($sql);
-            $stmt->execute([':quantity'=>$quantity,':cart_id'=>$cart_id]);
-        }
-
-    }
-    catch(PDOexception $e)
-    {
-        echo "Connection to database failed: " . $e->getMessage();
-    }
-}
-
-    }else{
-        $product_id=$_POST['Product_ID'];
-        $customer_id=$_POST['Customer_ID'];
-    }
+	// Print Table
+	$shopping_cart_rs = $pdo->prepare("SELECT * FROM Shopping_Cart WHERE Customer_ID = :Customer_ID");
+	$shopping_cart_rs->execute(array(":Customer_ID" => $customer_id));
+	$shopping_cart = $shopping_cart_rs->fetchAll(PDO::FETCH_ASSOC);
 
 
+	echo "Your shopping cart<br />";
+	foreach($shopping_cart as $item)
+	{
+		echo "<table border=2 cellspacing=2>";
+		echo "<tr><th>Product Name</th>";
+		echo "<th>Quantity</th>";
+		echo "<th>Remove from Cart</th></tr>";
 
-}
+		$item_name_rs = $pdo->prepare("SELECT Product_Name FROM Product WHERE Product_ID = :Product_ID");
+		$item_name_rs->execute(array(":Product_ID" => $item["Product_ID"]));
+		$item_name = $item_name_rs->fetch(PDO::FETCH_ASSOC);
 
+		echo "<tr><td>" . $item_name["Product_Name"]; . "</td>";
+		echo "<td>" . $item["Quantity"] . "</td>";
+		echo "<td><form action=\"shopping_cart2.php\" method=\"POST\">";
+		echo "<input type=\"hidden\" name=\"Product_ID\" value=\"" . $item["Product_ID"] . "\"/>";
+		echo "<input type=\"hidden\" name=\"Customer_ID\" value=\"" . $customer_id . "\"/>";
+		echo "<input type=\"submit\" value=\"Remove\"/>";
+		echo "</form></td></tr>";
+	}
 
+	echo "</table>";
 
-try
-{
-
-
-//echo $product_id.'--'.$customer_id;die;
-    // Insert into  Cart/wishlist table
-    if($product_id!='' && $customer_id!=''){
-
-        $sql = "INSERT INTO cart (Product_ID,Customer_ID,quantity) VALUES (?,?,?)";
-        $stmt= $pdo->prepare($sql);
-        $stmt->execute([$product_id, $customer_id,'1']);
-    }
-
-
-    // Get Cart/wishlist information
-    $item_info_rs = $pdo->prepare("SELECT * FROM cart as w JOIN Product as p ON w.Product_ID=p.Product_ID  WHERE w.Customer_ID = :customer;");
-    $item_info_rs->execute(array(':customer'=>$customer_id));
-    $item_info = $item_info_rs->fetchAll(PDO::FETCH_ASSOC);
-
-   // echo "<pre>";print_r($item_info);die;
-}
-catch(PDOexception $e)
-{
-echo "Connection to database failed: " . $e->getMessage();
-}
-
-?>
-
-<html><head><title>Shopping Cart</head></title>
-<body>
-
-<?php if(!empty($item_info)){?>
-    <table border=2 cellspacing=2>
-        <thead>
-        <th>Product Name</th>
-        <th>Details</th>
-        <th>Price</th>
-        <th>Size</th>
-        <th>Color</th>
-        <th>Quantity</th>
-        <th>Add</th>
-        <th>Remove</th>
-        </thead>
-        <tbody>
-        <?php
+	echo "<form action=\"checkout.php\" method=\"POST\">";
+	echo "<input type=\"hidden\" name=\"Customer_ID\" value=\"" . $_POST["Customer_ID"] . "\"/>";
+	echo "<input type=\"submit\" value=\"Checkout\"/>";
+	echo "</form><br /><br />";
 
 
-        foreach ($item_info as $cart){?>
-    <tr>
-        <td><?=$cart['Product_Name']?></td>
-        <td><?=$cart['Details']?></td>
-        <td><?=$cart['Base_Price']?></td>
-        <td><?=$cart['Size']?></td>
-        <td><?=$cart['Color']?></td>
-        <td><?=$cart['quantity']?></td>
-        <td>
-            <form method="post" action="">
-                <input type="hidden" name="product_id" value="<?=$cart['Product_ID']?>">
-                <input type="hidden" name="cart_id" value="<?=$cart['cart_id']?>">
-                <input type="hidden" name="quantity" value="<?=$cart['quantity']?>">
-                <button type="submit" name="submit" value="add_quantity">Add</button>
-            </form>
-        </td>
-        <td>
-            <form method="post" action="">
-                <input type="hidden" name="product_id" value="<?=$cart['Product_ID']?>">
-                <input type="hidden" name="cart_id" value="<?=$cart['cart_id']?>">
-                <input type="hidden" name="quantity" value="<?=$cart['quantity']?>">
-                <button type="submit"  name="submit"  value="remove">Remove</button>
-            </form>
-        </td>
-
-    </tr>
-        <?php } ?>
-        </tbody>
-    </table>
-<?php } else{ ?>
-
-<p>You cart is empty please shop from <a href="add_to_cart_confirm.php"> here</a> </p>
-<?php } ?>
-
-
-
-</body>
-</html>
