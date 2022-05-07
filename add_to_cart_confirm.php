7637ca3f4cee44be8a74f7c29b1eca6b9e9d1177
Michael Talusan <z1714949@hopper.cs.niu.edu>
2022-05-06 21:02:49 -0500

    Added logic to search for correct Product_ID when color and size are supplied

--- add_to_cart_confirm.php
+++ add_to_cart_confirm.php
@@ -2,10 +2,11 @@
 <body>
 <?php
 
-	include("secrets.php");
+	include("credentials.php");
 	
-	try{
-		$dsn = "mysql:host=courses;dbname=z1960742";
+	try
+	{
+		$dsn = "mysql:host=courses;dbname=z1714949";
 		$pdo = new PDO($dsn, $username, $password);
 		$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 	}
@@ -14,20 +15,19 @@
 		echo "Connection to database failed: " . $e->getMessage();
 	}
 
-	$C_ID = $_POST["Customer_ID"];
 	// Home button
-	echo "<form action='index.php' method='POST'>";
-	echo "<input type='hidden' name='Customer_ID' value=$C_ID />";
+	echo "<form action=\"index.php\" method=\"POST\">";
+	echo "<input type=\"hidden\" name=\"Customer_ID\" value=\"" . $_POST["Customer_ID"] . "\"/>";
 	echo "<input type=\"submit\" value=\"Home\"/>";
 	echo "</form><br /><br />";
- 	
+	
 	@$item_size = $_POST["Size"];
 	@$item_color = $_POST["Color"];
 	$item_quantity = $_POST["Quantity"];
 	$C_ID = $_POST["Customer_ID"];
 	$item_ID = $_POST["Product_ID"];
 	$item_name = $_POST["Product_Name"];
-	
+
 	// Print out item info
 	echo "Confirm addition to cart<br />";
 	echo $item_quantity . " x " . $item_name . "<br />";
@@ -47,34 +47,52 @@
 	echo "<br />";
 
 	// Search for correct ID if color or size are applicable
-	if(!@$item_size || !@$item_color)
+	if(@$item_size || @$item_color)
 	{
-		$item_ID_rs = $pdo->prepare("SELECT Product_ID FROM Product WHERE Product_Name = :Product_Name AND Size = :Size AND Color = :Color;");
-		$item_ID_rs->execute(array(":Product_Name" => $item_name, ":Size" => @$item_size, ":Color" => @$item_color));
-		$item_ID = $item_ID_rs->fetch(PDO::FETCH_ASSOC);
-/*		// Submit here
-		echo "<form action='shopping_cart.php' method='POST'>";
-		foreach($item_ID as $item)
+		// Search if only size applies
+		if(@$item_size && !@$item_color)
 		{
-			// Data to pass
-	//		echo "<input type='hidden' name= 'Quantity' value= $item_quantity />";
-			echo "<input type='hidden' name='Product_ID' value= $item/>";
-	//		echo "<input type='hidden' name='Customer_ID' value= $C_ID/>";
-			echo "<input type='submit' value='Confirm'/>";
-			echo "</form>";
+			$item_ID_rs = $pdo->prepare("SELECT Product_ID FROM Product WHERE Product_Name = :Product_Name AND Size = :Size;");
+			$item_ID_rs->execute(array(":Product_Name" => $item_name, ":Size" => @$item_size));
+			$item_ID = $item_ID_rs->fetch(PDO::FETCH_ASSOC);
 		}
- */	}
-	
-	// Submit here
-	echo "<form action='shopping_cart.php' method='POST'>";
-	
-	// Data to pass
-	echo "<input type='hidden' name= 'Quantity' value= $item_quantity />";
-	echo "<input type='hidden' name='Product_ID' value= $item_ID/>";
-	echo "<input type='hidden' name='Customer_ID' value= $C_ID/>";
-	echo "<input type='submit' value='Confirm'/>";
-	echo "</form>";
- ?>
 
+		// Search if only color applies
+		if(@$item_color && !@$item_size)
+		{
+			$item_ID_rs = $pdo->prepare("SELECT Product_ID FROM Product WHERE Product_Name = :Product_Name AND Color = :Color;");
+			$item_ID_rs->execute(array(":Product_Name" => $item_name, ":Color" => @$item_color));
+			$item_ID = $item_ID_rs->fetch(PDO::FETCH_ASSOC);
+		}
+
+		// Search if both apply
+		if(@$item_color && @$item_size)
+		{
+			$item_ID_rs = $pdo->prepare("SELECT Product_ID FROM Product WHERE Product_Name = :Product_Name AND (Size = :Size AND Color = :Color);");
+			$item_ID_rs->execute(array(":Product_Name" => $item_name, ":Size" => @$item_size, ":Color" => @$item_color));
+			$item_ID = $item_ID_rs->fetch(PDO::FETCH_ASSOC);
+		}
+	}
+	else
+	{
+		// Search if none apply
+		$item_ID_rs = $pdo->prepare("SELECT Product_ID FROM Product WHERE Product_Name = :Product_Name;");
+		$item_ID_rs->execute(array(":Product_Name" => $item_name));
+		$item_ID = $item_ID_rs->fetch(PDO::FETCH_ASSOC);
+	}
+
+
+	// Submit here
+	echo "<form action=\"shopping_cart.php\" method=\"POST\">";
+
+	// Data to pass
+	echo "<input type=\"hidden\" name=\"Quantity\" value=\"" . $item_quantity . "\" />";
+	echo "<input type=\"hidden\" name=\"Product_ID\" value=\"" . $item_ID["Product_ID"] . "\" />";
+	echo "<input type=\"hidden\" name=\"Customer_ID\" value=\"" . $C_ID . "\" />";
+
+	echo "<input type=\"submit\" value=\"Confirm\">";
+
+	echo "</form>";
+?>
 </body>
 </html>
