5ac099903e108639db3b7de8372a8bab693cc830
Michael Talusan <z1714949@hopper.cs.niu.edu>
2022-05-06 21:10:07 -0500

    Fixed sql syntax errors

--- shopping_cart.php
+++ shopping_cart.php
@@ -1,72 +1,76 @@
-<html><head><title>Shopping Cart</title></head><body><pre>
 <?php
-	$C_ID = $_POST["Customer_ID"];
 
 	echo "<form action=\"index.php\" method=\"POST\">";
 	echo "<input type=\"hidden\" name=\"Customer_ID\" value=\"" . $_POST["Customer_ID"] . "\"/>";
 	echo "<input type=\"submit\" value=\"Home\"/>";
 	echo "</form><br /><br />";
 
-	$Product_ID = $_POST["Product_ID"];
-	$Quantity = $_POST["Quantity"];
 
-	include("secrets.php");
+	$customer_id = $_POST["Customer_ID"];
+	@$product_id = $_POST["Product_ID"];
+	@$quantity = @$_POST['Quantity'];
 
-	$dsn = "mysql:host=courses;dbname=z1960742";
+	include("credentials.php");
+
+	$dsn = "mysql:host=courses;dbname=z1714949";
 	$pdo = new PDO($dsn, $username, $password);
 	$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
+	
 	// Add to cart
-	if($Quantity)
+	if(@$quantity)
 	{
-		$sql = "UPDATE Shopping_Cart SET Quantity = ? WHERE Product_ID = ? ANd Customer_ID = ?";		
-			
-	//	$sql = "INSERT INTO Shopping_Cart (Product_ID, Customer_ID, Quantity) VALUES (?, ?, ?)";
+		$sql = "INSERT INTO Shopping_Cart (Product_ID, Customer_ID, Quantity) VALUES (:Product_ID, :Customer_ID, :Quantity)";
 		$stmt= $pdo->prepare($sql);
-		$stmt->execute(array($Quantity, $Product_ID, $C_ID));
+		$stmt->execute(array(':Product_ID' => @$product_id, ':Customer_ID' => $customer_id, ':Quantity'=> @$quantity));
 	}
-	else if($Product_ID)
+	else if(@$product_id)
 	{
 		// Remove from cart
-		$remove_rs = $pdo->prepare("DELETE FROM Shopping Cart WHERE Customer_ID = :Customer_ID AND Product_ID = :Product_ID");
-		$remove_rs->execute(array(":Customer_ID" => $C_ID, ":Product_ID" => $Product_ID));
-		echo $Product_ID . " was removed from the shopping cart due to shortage in stock quantity";
+		$remove_rs = $pdo->prepare("DELETE FROM Shopping_Cart WHERE Customer_ID = :Customer_ID AND Product_ID = :Product_ID;");
+		$remove_rs->execute(array(":Customer_ID" => $customer_id, ":Product_ID" => @$product_id));
 
 	}
 
 	// Print Table
 	$shopping_cart_rs = $pdo->prepare("SELECT * FROM Shopping_Cart WHERE Customer_ID = :Customer_ID");
-	$shopping_cart_rs->execute(array(":Customer_ID" => $C_ID));
+	$shopping_cart_rs->execute(array(":Customer_ID" => $customer_id));
 	$shopping_cart = $shopping_cart_rs->fetchAll(PDO::FETCH_ASSOC);
-	
-	echo "Your shopping cart<br />";
-	echo "<table border=2 cellspacing=3>";
-//	echo "<tr><th>Product ID</th>";
-//	echo "<th>Quantity</th>";
 
-	foreach($shopping_cart as $row)
-	{	
-		echo "<tr>";
-		foreach($row as $item)
+
+	if($shopping_cart)
+	{
+		echo "Your shopping cart<br />";
+		foreach($shopping_cart as $item)
 		{
-			echo "<td>$item</td>";
+			echo "<table border=2 cellspacing=2>";
+			echo "<tr><th>Product Name</th>";
+			echo "<th>Quantity</th>";
+			echo "<th>Remove from Cart</th></tr>";
+
+			$item_name_rs = $pdo->prepare("SELECT Product_Name FROM Product WHERE Product_ID = :Product_ID");
+			$item_name_rs->execute(array(":Product_ID" => $item["Product_ID"]));
+			$item_name = $item_name_rs->fetch(PDO::FETCH_ASSOC);
+
+			echo "<tr><td>" . $item_name["Product_Name"] . "</td>";
+			echo "<td>" . $item["Quantity"] . "</td>";
+			echo "<td><form action=\"shopping_cart.php\" method=\"POST\">";
+			echo "<input type=\"hidden\" name=\"Product_ID\" value=\"" . $item["Product_ID"] . "\"/>";
+			echo "<input type=\"hidden\" name=\"Customer_ID\" value=\"" . $customer_id . "\"/>";
+			echo "<input type=\"submit\" value=\"Remove\"/>";
+			echo "</form></td></tr>";
 		}
 
-/*		echo "<td><form action=\"shopping_cart.php\" method=\"POST\">";
-		echo "<input type=\"hidden\" name=\"Product_ID\" value=\"" . $item['Product_ID'] . "\"/>";
-		echo "<input type=\"hidden\" name=\"Customer_ID\" value=\"" . $C_ID. "\"/>";
-		echo "<input type=\"submit\" value=\"Remove\"/>";
-		echo "</form></td></tr>";
- */		echo "</tr>";		
+		echo "</table>";
+
+		echo "<form action=\"checkout.php\" method=\"POST\">";
+		echo "<input type=\"hidden\" name=\"Customer_ID\" value=\"" . $_POST["Customer_ID"] . "\"/>";
+		echo "<input type=\"submit\" value=\"Checkout\"/>";
+		echo "</form><br /><br />";
 	}
-	echo "</table>";
+	else
+	{
+		echo "Your shopping cart is empty";
+	}
 
 
-	echo "<form action=\"checkout.php\" method=\"POST\">";
-	echo "<input type=\"hidden\" name=\"Customer_ID\" value=\"" . $_POST['Customer_ID'] . "\"/>";
-	echo "<input type=\"submit\" value=\"Checkout\"/>";
-	echo "</form><br /><br />";
-
-?>
-
-</pre></body></html>
