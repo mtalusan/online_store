7ebe5d66aee8debbfd25b64300cc6e0594692d95
Michael Talusan <z1714949@hopper.cs.niu.edu>
2022-05-06 21:08:48 -0500

    Links to item_page and remove from wishlist now pass Customer_ID. Fixed variable names. Checks if item is on wishlist before adding

--- wishlist.php
+++ wishlist.php
@@ -34,12 +34,23 @@
 		$remove_rs->execute(array(":Product_ID" => @$remove_item_ID, ":Customer_ID" => $C_ID));
 	}
 
-	// If this page was passed an item_ID, add
-	// it to the wishlist
+	// If this page was passed an item_ID, check if it's already wishlisted
+	// and add it to the wishlist if it isn't
 	if(@$item_ID != NULL)
 	{
-		$add_rs = $pdo->prepare("INSERT INTO Wishlist VALUES(:item_ID, :Customer_ID);");
-		$add_rs->execute(array(":item_ID" => @$item_ID, ":Customer_ID" => $C_ID));
+		// Check if its already on the list
+		// This is pretty much a copy of the query done below
+		// that fills the table bc the wishlist at this point
+		// doesn't have the new item that was submitted to be added
+		$item_check_rs = $pdo->prepare("SELECT * FROM Wishlist WHERE Customer_ID = :Customer_ID AND Product_ID = :Product_ID;");
+		$item_check_rs->execute(array(":Customer_ID" => $C_ID, ":Product_ID" => @$item_ID));
+		$item_check = $item_check_rs->fetchALL(PDO::FETCH_ASSOC);
+
+		if(!$item_check)
+		{
+			$add_rs = $pdo->prepare("INSERT INTO Wishlist (Product_ID, Customer_ID) VALUES(:item_ID, :Customer_ID);");
+			$add_rs->execute(array(":item_ID" => @$item_ID, ":Customer_ID" => $C_ID));
+		}
 	}
 
 	// Fetch the customer's wishlist
@@ -60,7 +71,7 @@
 			// Run second query to get Product_Name
 			$item_name_rs = $pdo->prepare("SELECT Product_Name FROM Product WHERE Product_ID = :Product_ID");
 			$item_name_rs->execute(array(":Product_ID" => $item["Product_ID"]));
-			$item_name = $item_name_rs->fetchAll(PDO::FETCH_ASSOC);
+			$item_name = $item_name_rs->fetch(PDO::FETCH_ASSOC);
 
 			// Print Product_Name, link to item_page.php, and button
 			// to remove item from wishlist
@@ -68,13 +79,15 @@
 
 			// Link to store page
 			echo "<td><form action=\"item_page.php\" method=\"POST\">";
-			echo "<input type=\"hidden\" name=\"item_ID\" value=\"" . $item["Product_ID"] . "\"/>";
+			echo "<input type=\"hidden\" name=\"Product_ID\" value=\"" . $item["Product_ID"] . "\"/>";
+			echo "<input type=\"hidden\" name=\"Customer_ID\" value=\"" . $C_ID . "\"/>";
 			echo "<input type=\"submit\" value=\"Buy\"/>";
 			echo "</td></form>";
 
 			// Remove this item from wishlist button
 			echo "<td><form action=\"wishlist.php\" method=\"POST\">";
 			echo "<input type=\"hidden\" name=\"remove_item_ID\" value=\"" . $item["Product_ID"] . "\"/>";
+			echo "<input type=\"hidden\" name=\"Customer_ID\" value=\"" . $C_ID . "\"/>";
 			echo "<input type=\"submit\" value=\"Remove from List\"/>";
 			echo "</td></form>";
 			echo "</tr>";
